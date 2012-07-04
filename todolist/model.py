32ab1e5d27537d6c810c2609f599383ca3f059e4
aiisahik <jie.z.zhou@gmail.com>
2012-07-03 21:01:44 -0700

    added jquery

--- todolist/model.py
+++ todolist/model.py
@@ -25,7 +25,7 @@ class Task(db.Model):
     done = db.Column(db.Boolean, default=False, nullable = False) 
     created_at = db.Column(db.DateTime, nullable = False) 
     completed_at = db.Column(db.DateTime, default=None, nullable = True) 
-    priority = db.Column(db.Integer, default=1, nullable = False)
+    # priority = db.Column(db.Integer, default=1, nullable = False)
 
     def __init__(self, title, notes=''):
         self.title = title
